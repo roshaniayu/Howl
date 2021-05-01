06c7bf6a252d6644dbe98b152346fd1a03617c1f
roshaniayu <asti.roshani@gmail.com>
2021-05-01 12:43:33 +0700

    minor change on home page

--- Howl/ViewController.swift
+++ Howl/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerPicker: UIPickerView!
+    @IBOutlet weak var timerView: UIView!
+    
+    let timer = ["5 mins", "10 mins", "15 mins", "30 mins", "45 mins", "1 hour"]
+    let pickerWidth: CGFloat = 62
+    let pickerHeight: CGFloat = 80
+    var rotationAngle: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // timerPicker modification
+        rotationAngle = -90 * (.pi/180)
+        timerPicker.transform = CGAffineTransform(rotationAngle: rotationAngle)
+        
+        timerPicker.frame = CGRect(x: 64, y: 300, width: 260, height: pickerWidth)
+        self.view.addSubview(timerPicker)
+        
+        // timerView modification
+        timerView.layer.cornerRadius = 22
     }
-
-
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timer.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return pickerHeight
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerWidth
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: pickerWidth, height: pickerHeight)
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 0, y: 0, width: pickerWidth, height: pickerHeight)
+        label.textAlignment = .center
+        label.font = UIFont(name: "NunitoSans-Regular.ttf", size: 17)
+        label.textColor = UIColor(red:0.82, green:0.82, blue:0.80, alpha:1.0)
+        label.text = timer[row]
+        view.addSubview(label)
+        
+        rotationAngle = 90 * (.pi/180)
+        view.transform = CGAffineTransform(rotationAngle: rotationAngle)
+        
+        return view
+    }
+}
