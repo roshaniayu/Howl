b204c2a9a01d640b619d4432943eb0e0f81b939b
roshaniayu <asti.roshani@gmail.com>
2021-05-03 02:30:19 +0700

    add functionality to play song

--- Howl/ViewController.swift
+++ Howl/ViewController.swift
@@ -5,10 +5,11 @@
 //  Created by Roshani Ayu Pranasti on 29/04/21.
 //
 
-import UIKit
+import AVFoundation
 import CoreData
+import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet weak var timerPicker: UIPickerView!
     @IBOutlet weak var timerView: UIView!
@@ -20,23 +21,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var countdownView: UIView!
+    @IBOutlet weak var countdownLabel: UILabel!
     
     let userDefaultsValue = UserDefaults.standard.getValueLoad()
     let pickerWidth: CGFloat = 62
     let pickerHeight: CGFloat = 80
     var rotationAngle: CGFloat!
-    var timer: [Time] = []
-    var songs = [Song]()
-    var selectedSong = [Song]()
+    var times: [Time] = []
+    var songs: [Song] = []
+    var selectedTime: Int = 299
+    var selectedSong: [Song] = []
+    var timer: Timer?
+    var player: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+                
         // timerPicker modification
         rotationAngle = -90 * (.pi/180)
         timerPicker.transform = CGAffineTransform(rotationAngle: rotationAngle)
-        
         timerPicker.frame = CGRect(x: 64, y: 300, width: 260, height: pickerWidth)
         self.view.addSubview(timerPicker)
         
@@ -48,12 +52,12 @@ class ViewController: UIViewController {
     }
     
     func generateTime() {
-        timer.append(Time(timeType: .fiveMins, timeName: "5 mins", timeDuration: 300))
-        timer.append(Time(timeType: .tenMins, timeName: "10 mins", timeDuration: 600))
-        timer.append(Time(timeType: .fifteenMins, timeName: "15 mins", timeDuration: 900))
-        timer.append(Time(timeType: .thirtyMins, timeName: "30 mins", timeDuration: 1800))
-        timer.append(Time(timeType: .fortyfiveMins, timeName: "45 mins", timeDuration: 2700))
-        timer.append(Time(timeType: .oneHour, timeName: "1 hour", timeDuration: 3600))
+        times.append(Time(timeType: .fiveMins, timeName: "5 mins", timeDuration: 299))
+        times.append(Time(timeType: .tenMins, timeName: "10 mins", timeDuration: 599))
+        times.append(Time(timeType: .fifteenMins, timeName: "15 mins", timeDuration: 899))
+        times.append(Time(timeType: .thirtyMins, timeName: "30 mins", timeDuration: 1799))
+        times.append(Time(timeType: .fortyfiveMins, timeName: "45 mins", timeDuration: 2699))
+        times.append(Time(timeType: .oneHour, timeName: "1 hour", timeDuration: 3599))
     }
     
     func getCoreDataContainer() -> NSManagedObjectContext {
@@ -74,7 +78,7 @@ class ViewController: UIViewController {
         do {
             selectedSong = try context.fetch(fetchRequest)
         } catch {
-            print("Error fetching data from context \(error)")
+            print("Error fetching data from context: \(error)")
         }
     }
     
@@ -121,9 +125,9 @@ class ViewController: UIViewController {
     
     func loadSongsData() {
         if userDefaultsValue == nil {
-            insertItem(audio: "02 Merry Go Round of Life (From _Howl's Moving Castle_)", background: "mountain", character: "howl_and_sophie", selected: true, title: "Merry Go Round of Life")
-            insertItem(audio: "14 Kaze no Toori Michi (From _My Neighbor Totoro_)", background: "rain", character: "totoro", selected: false, title: "Path of the Wind")
-            insertItem(audio: "01 Inochi no Namae (From _Spirited Away_)", background: "clouds", character: "spirited_away", selected: false, title: "The Name of Life")
+            insertItem(audio: "merry_go_round_of_life", background: "mountain", character: "howl_and_sophie", selected: true, title: "Merry Go Round of Life")
+            insertItem(audio: "kaze_no_toori_michi", background: "rain", character: "totoro", selected: false, title: "Path of the Wind")
+            insertItem(audio: "inochi_no_namae", background: "clouds", character: "spirited_away", selected: false, title: "The Name of Life")
             
             UserDefaults.standard.setValueLoad(value: true)
         }
@@ -131,7 +135,7 @@ class ViewController: UIViewController {
         loadAmbience()
     }
     
-    @IBAction func playSong(_ sender: UIButton) {
+    func configureInterface() {
         headerLabel.text = "Have a great sleep"
         descriptionLabel.text = "Once upon a time th..e.... a.. zzzzzzzzz..."
         descriptionLabel.frame = CGRect(x: 97, y: 223, width: 197, height: 48)
@@ -143,6 +147,70 @@ class ViewController: UIViewController {
         playButton.setImage(UIImage(named: "icon_stop"), for: .normal)
         countdownView.layer.cornerRadius = 19
         countdownView.isHidden = false
+        countdownLabel.text = formatTimer(selectedTime)
+    }
+    
+    func formatTimer(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds/60) % 60
+        
+        return String(format: "-%02d:%02d", minutes, seconds)
+    }
+    
+    @objc func updateTimer() {
+        countdownLabel.text = formatTimer(selectedTime)
+        
+        if selectedTime != 0 {
+            selectedTime -= 1
+        } else {
+            if let timer = timer {
+                timer.invalidate()
+                self.timer = nil
+            }
+            if let player = player {
+                player.stop()
+                self.player = nil
+            }
+        }
+    }
+    
+    func configureTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    func configureSong() {
+        let song = selectedSong[0]
+        let urlString = Bundle.main.path(forResource: song.audio, ofType: "m4a")
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            guard let urlString = urlString else {
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            guard let player = player else {
+                return
+            }
+            
+            player.delegate = self
+            player.play()
+        } catch {
+            print("Error playing the song: \(error)")
+        }
+    }
+    
+    @IBAction func playSong(_ sender: UIButton) {
+        configureInterface()
+        configureTimer()
+        configureSong()
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if selectedTime != 0 && flag == true {
+            player.play()
+        }
     }
 }
 
@@ -152,7 +220,7 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return timer.count
+        return times.count
     }
     
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
@@ -172,12 +240,17 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         label.textAlignment = .center
         label.font = UIFont(name: "NunitoSans-Regular.ttf", size: 17)
         label.textColor = UIColor(red: 0.82, green: 0.82, blue: 0.80, alpha: 1.0)
-        label.text = timer[row].name
+        label.text = times[row].name
         view.addSubview(label)
         
         rotationAngle = 90 * (.pi/180)
         view.transform = CGAffineTransform(rotationAngle: rotationAngle)
         
         return view
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedRow = times[row]
+        selectedTime = selectedRow.duration!
     }
 }
